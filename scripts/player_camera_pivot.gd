8c52c7a09126b5fa456ac85ab51ba999912b02b0
redru <luzenobi@gmail.com>
2024-11-09 22:10:33 +0100

    camera movement improved

--- scripts/player_camera_pivot.gd
+++ scripts/player_camera_pivot.gd
@@ -1,19 +1,36 @@
 extends Node3D
 
-@export var rotation_sens := 0.01
+@export var rotation_sens := 0.5
 
 var rotating := false
+var current_lock_rotation := Vector3.ZERO
+var current_mouse_relative:= Vector2.ZERO
+
+func _process(delta: float):
+	# Handle rotation state
+	if rotating:
+		# Mouse movement
+		if current_mouse_relative:
+			rotate(Vector3.DOWN, deg_to_rad(current_mouse_relative.x * rotation_sens))
+			rotate(Vector3.RIGHT, deg_to_rad(current_mouse_relative.y * rotation_sens))
+			current_lock_rotation = global_rotation
+		# Frozen camera position
+		else:
+			global_rotation = current_lock_rotation
+			
+	if current_mouse_relative:
+		current_mouse_relative = Vector2.ZERO
 
 func _input(event: InputEvent):
+	# Enable / disable rotating
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
+		if !rotating and event.pressed:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			rotating = true
+			current_lock_rotation = global_rotation
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			rotating = false
-	
-	if event is InputEventMouseMotion and rotating:
-		rotate(Vector3.UP, deg_to_rad(event.screen_velocity.x * rotation_sens))
-		rotate(Vector3.LEFT, deg_to_rad(event.screen_velocity.y * rotation_sens))
-		
+			
+	if event is InputEventMouseMotion:
+		current_mouse_relative = event.relative
