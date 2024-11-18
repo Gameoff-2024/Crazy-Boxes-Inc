a45a8c56d7bd79b2efa331855fb605926f037be5
andrea.rubino <andrea.rubino@welld.ch>
2024-11-18 09:35:20 +0100

    fix: skate quest

--- scripts/import_asset.gd
+++ scripts/import_asset.gd
@@ -1,8 +1,5 @@
 @tool
 extends EditorScenePostImport
 
-func _post_import(scene):
-	var mesh_instance: MeshInstance3D = scene.get_child(0).get_child(0)
-	mesh_instance.position = Vector3(0.0, 0.0, 0.0)
-	mesh_instance.scale = Vector3(5.5, 5.5, 5.5)
+func post_import(scene):
 	return scene
