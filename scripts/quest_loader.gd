27a3a6b88426c88db6d011898689cb414a6e0090
Andrea Rubino <24411828+rubin0@users.noreply.github.com>
2024-11-28 10:55:28 +0100

    feat: tree quest

--- scripts/quest_loader.gd
+++ scripts/quest_loader.gd
@@ -1,6 +1,6 @@
 class_name QuestLoader
 
-@export var quest_path = "res://resources/quests/"
+static var quest_path = "res://resources/quests/"
 
 var quests_list: Array
 var shuffled = false
@@ -9,6 +9,9 @@ func _init():
 	quests_list = Array(DirAccess.get_files_at(quest_path))
 	quests_list.shuffle()
 	
+
+static func get_total_quests() -> int:
+	return Array(DirAccess.get_files_at(quest_path)).size()
 
 func choose_random_quest() -> Quest:
 	if quests_list.is_empty():
